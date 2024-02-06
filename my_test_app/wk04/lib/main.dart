0b53382880ede956cb8285be8ce77cdfcec9d2b1
Tianming Liu <ucfntl0@ucl.ac.uk>
2024-02-06 12:30:04 +0000

    Test Code for Flutter

--- my_test_app/wk04/lib/main.dart
+++ my_test_app/wk04/lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
+    final wordPair02 = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to My Flutter'),
+          title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World!!'),
-        ),
+        
+        body: Center(// Change "const" to "new".
+            child : Text(wordPair.asCamelCase)),
       ),
     );
   }
