7b9dacbb914478f8feb4eb7f1294eda285b7ce31
Tianming Liu <ucfntl0@ucl.ac.uk>
2024-02-27 12:44:35 +0000

    Finish the loading logic

--- geotracker/lib/main.dart
+++ geotracker/lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:geotracker/start_screen.dart';
+import 'package:geotracker/start_page.dart';
 
 void main() {
-  runApp(const Geotracker());
-}
-
-class Geotracker extends StatelessWidget {
-  const Geotracker({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: StartScreen(),
-      ),
-    );
-  } 
+  runApp(const StartPage());
 }
