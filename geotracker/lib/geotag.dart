8f0e66c1bf87856d6a5917eea98bd3e6da74df3c
Tianming Liu <ucfntl0@ucl.ac.uk>
2024-03-24 12:16:37 +0000

    Update Core Widget Geotags

---
+++ geotracker/lib/geotag.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GeoTags extends StatefulWidget {
+  const GeoTags({super.key});
+
+  @override
+  State<GeoTags> createState() => _GeoTagsState();
+}
+
+class _GeoTagsState extends State<GeoTags> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(); 
+  }
+}
