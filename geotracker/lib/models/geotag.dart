9c51aef3c4320f07b68ecfea8e6cc7ee2fd4c206
Tianming Liu <ucfntl0@ucl.ac.uk>
2024-03-24 12:10:29 +0000

    Create the geotag template class

---
+++ geotracker/lib/models/geotag.dart
@@ -0,0 +1,8 @@
+class GeoTag {
+  GeoTag({required this.id, required this.type, required this.time, required this.info});
+
+  final String id;
+  final String type;
+  final DateTime time;
+  final String info;
+}
