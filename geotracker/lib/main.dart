7b796b108b46c9b460e94e36f1470eeda26cebe3
Tianming Liu <ucfntl0@ucl.ac.uk>
2024-03-25 10:29:54 +0000

    Update the TAG Page

--- geotracker/lib/main.dart
+++ geotracker/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geotracker/start_page.dart';
+import 'package:geotracker/widgets/start_page.dart';
 
 void main() {
   runApp(const StartPage());
--- geotracker/lib/models/geotag.dart
+++ geotracker/lib/models/geotag.dart
@@ -1,13 +1,40 @@
-import 'package:uuid/data.dart';
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 const uuid = Uuid();
 
-class GeoTag {
-  GeoTag({required this.type, required this.time, required this.info}) : id = uuid.v4();
+final formatter = DateFormat.yMd().add_jm();
 
+enum Category { food, travel, leisure, work }
+
+enum TagType { place, route }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.house,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class GeoTag {
+  GeoTag(
+      {required this.category,
+      required this.tagType,
+      required this.time,
+      required this.info})
+      : id = uuid.v4();
+  // unique id for each geotag
   final String id;
-  final String type;
+  // the category users choose when creating
+  final Category category;
+  // differentiate the place tag and route tag
+  final TagType tagType;
+  // record the creating time of each tag
   final DateTime time;
-  final String info; 
+  final String info;
+
+  String get formattedDate {
+    return formatter.format(time);
+  }
 }
