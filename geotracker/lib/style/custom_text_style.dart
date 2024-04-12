85a1ccc2243ba0258297427c37384d1279fde300
Tianming Liu <ucfntl0@ucl.ac.uk>
2024-04-12 18:12:13 +0100

    Create custom style file for app

---
+++ geotracker/lib/style/custom_text_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomTextStyle {
+  static TextStyle normalText = TextStyle(
+    fontFamily: GoogleFonts.roboto().fontFamily,
+    color: const Color.fromARGB(255, 0, 0, 0),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+}
